1a84106490e251b86a7dc2228a9ece0babc0a6d0
yokoo1 <ababys24a@gmail.com>
2020-01-13 23:58:18 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,8 +2,9 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+  test "should get root" do
+    get root_url
+    assert_response :success
   end
 
   test "should get home" do
